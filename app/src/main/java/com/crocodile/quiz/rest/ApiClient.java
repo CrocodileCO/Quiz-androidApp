a573894b8f26b5854e21173cd123cc569b7759ed
Geralt38 <geralt38@gmail.com>
2018-02-16 22:15:37 +0800

    Actual server PogChamp

--- app/src/main/java/com/crocodile/quiz/rest/ApiClient.java
+++ app/src/main/java/com/crocodile/quiz/rest/ApiClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    public static final String BASE_URL = "http://10.0.2.2:3000/api/";
+    public static final String BASE_URL = "https://eggdance.herokuapp.com/api/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
