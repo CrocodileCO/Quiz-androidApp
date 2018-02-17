8bfda7641518b4ba226842efdca513a852daa86b
verteks <sendmydog@mail.ru>
2018-02-17 21:20:05 +0800

    add custom toolbar

--- app/src/main/java/com/crocodile/quiz/activity/MainActivity.java
+++ app/src/main/java/com/crocodile/quiz/activity/MainActivity.java
@@ -1,29 +1,22 @@
 package com.crocodile.quiz.activity;
 
 import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.widget.Toast;
+import android.view.Menu;
 
 import com.crocodile.quiz.R;
 import com.crocodile.quiz.adapter.MenuAdapter;
 import com.crocodile.quiz.helper.DownloadHelper;
 import com.crocodile.quiz.model.Topic;
-import com.crocodile.quiz.model.TopicsResponse;
 import com.crocodile.quiz.rest.ApiClient;
 import com.crocodile.quiz.rest.ServerInterface;
 import com.crocodile.quiz.rest.ServiceGenerator;
-import com.google.gson.Gson;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import retrofit2.Call;
@@ -41,9 +34,13 @@ public class MainActivity extends AppCompatActivity implements DownloadHelper.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        Toolbar mActionBarToolbar = (Toolbar) findViewById(R.id.toolbar_actionbar);
+        setSupportActionBar(mActionBarToolbar);
+
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-        String[] myDataset = getDataSet();
 
         mRecyclerView.setHasFixedSize(true);
 
@@ -83,13 +80,10 @@ public class MainActivity extends AppCompatActivity implements DownloadHelper.On
 
     }
 
-    private String[] getDataSet() {
-
-        String[] mDataSet = new String[10];
-        for (int i = 0; i < 10; i++) {
-            mDataSet[i] = "item" + i;
-        }
-        return mDataSet;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
     }
 
     @Override
